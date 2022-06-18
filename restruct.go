ae65a26c6059c0345fd21e582643d4e51b56c380
faisal <faisalraja@users.noreply.github.com>
2022-06-18 14:57:45 -0500

    fixed registration

--- restruct.go
+++ restruct.go
@@ -7,12 +7,9 @@ import (
 // Handle registers a struct  or a *Handler for the given pattern in the http.DefaultServeMux.
 func Handle(pattern string, svc interface{}) {
 	h, ok := svc.(*Handler)
-	if ok {
-		h.mustCompile(pattern)
-		http.Handle(pattern, h)
-		return
+	if !ok {
+		h = NewHandler(svc)
 	}
-	h = NewHandler(svc)
 	h.mustCompile(pattern)
-	http.Handle(pattern, h)
+	http.Handle(h.prefix, h)
 }
